1771e672274bb54f869be45144e89d44454aede6
Shuobin-P <3120465015@qq.com>
2022-04-15 22:41:25 +0800

    Front End:Get all data from database

--- src/main/java/com/google/wiki/service/EbookService.java
+++ src/main/java/com/google/wiki/service/EbookService.java
@@ -7,6 +7,7 @@ import com.google.wiki.req.EbookReq;
 import com.google.wiki.resp.EbookResp;
 import com.google.wiki.utils.CopyUtil;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -25,7 +26,9 @@ public class EbookService {
         //这个EbookExample是干什么的
         EbookExample ebookExample = new EbookExample();
         EbookExample.Criteria criteria = ebookExample.createCriteria();
-
+        if (!ObjectUtils.isEmpty(ebookReq.getName())) {
+            criteria.andNameLike("%" + ebookReq.getName() + "%");
+        }
         criteria.andNameLike("%" + ebookReq.getName() + "%");
 
         // 这里查出来的是Ebook，里面字段和表字段是一样的，但是我们返回出去的是响应封装类，因此要转化一下
